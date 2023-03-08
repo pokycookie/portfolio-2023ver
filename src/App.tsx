521dda25dd8d27044afd4671cdea3cab2753b3c2
PokyCookie <0cookieboy0@gmail.com>
2023-03-09 03:10:58 +0900

    app resize window

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+import Background from "./components/backgrounds/background";
+import Zodiac from "./components/backgrounds/zodiac/zodiac";
+
+interface IWindow {
+  width: number;
+  height: number;
+}
+
 function App() {
-  return <div className="App"></div>;
+  const [windows, setWindows] = useState<IWindow>({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const resizeHandler = (e: UIEvent) => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    setWindows({ width, height });
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", resizeHandler);
+    return () => {
+      window.removeEventListener("resize", resizeHandler);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <Background size={windows} />
+    </div>
+  );
 }
 
 export default App;
