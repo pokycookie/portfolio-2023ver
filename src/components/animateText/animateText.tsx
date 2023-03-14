edf0d1a978fef9a8e6c1b38333fc9f6fddf33c6c
PokyCookie <0cookieboy0@gmail.com>
2023-03-14 20:34:01 +0900

    animateText

--- src/components/animateText/animateText.tsx
+++ src/components/animateText/animateText.tsx
@@ -1,41 +1,61 @@
 import { useEffect, useState } from "react";
 import { useInterval } from "../../lib/hooks";
+import { _className } from "../../lib/basic";
+import "./animateText.css";
 
 interface IProps {
-  text: string;
+  text: string | string[];
   speed?: number;
+  interval?: number;
   fontSize?: number;
   color?: string;
+  className?: string;
 }
 
 function AnimateText(props: IProps) {
   const [text, setText] = useState("");
-  const [index, setIndex] = useState(0);
+  const [textIndex, setTextIndex] = useState(0);
+  const [arrIndex, setArrIndex] = useState(0);
 
   const color = props.color ?? "black";
   const fontSize = props.fontSize ?? 20;
+  const interval = props.interval ?? 5000;
+  const textSpeed = props.speed ?? 200;
+  const textLength =
+    typeof props.text === "string" ? props.text.length : props.text[arrIndex].length;
 
   useInterval(
     () => {
-      if (index >= props.text.length) {
+      if (textIndex === textLength) {
         setText("");
-        setIndex(0);
+        setTextIndex(0);
+        if (typeof props.text !== "string") {
+          if (arrIndex === props.text.length - 1) {
+            setArrIndex(0);
+          } else {
+            setArrIndex((prev) => prev + 1);
+          }
+        }
       } else {
-        const tmpText = text.concat(props.text[index]);
+        const tmpText =
+          typeof props.text === "string"
+            ? text.concat(props.text[textIndex])
+            : text.concat(props.text[arrIndex][textIndex]);
         setText(tmpText);
-        setIndex(index + 1);
+        setTextIndex(textIndex + 1);
       }
     },
-    index >= props.text.length ? 5000 : props.speed ?? 200
+    textIndex >= textLength ? interval : textSpeed
   );
 
   useEffect(() => {
     setText("");
-    setIndex(0);
+    setTextIndex(0);
+    setArrIndex(0);
   }, [props.text]);
 
   return (
-    <div className="animate__text">
+    <div className={_className("animate__text", props.className)}>
       <p style={{ color, fontSize }}>{text}</p>
     </div>
   );
