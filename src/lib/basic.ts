bb199c212a741f2f391a770586ac85c94c593335
PokyCookie <0cookieboy0@gmail.com>
2023-03-15 23:11:07 +0900

    lib

--- src/lib/basic.ts
+++ src/lib/basic.ts
@@ -9,3 +9,9 @@ export function _range(end: number, start: number = 0, step: number = 1) {
 export function _random(max: number, min: number = 0) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+export function _className(defaultClass: string, addClass?: string) {
+  let className = defaultClass;
+  if (addClass) className = className.concat(` ${addClass}`);
+  return className;
+}
--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 
 export function useInterval(callback: () => void, delay: number | null) {
   const savedCallback = useRef(callback);
@@ -14,6 +14,23 @@ export function useInterval(callback: () => void, delay: number | null) {
     if (delay !== null) {
       let id = setInterval(tick, delay);
       return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+export default function useTimeout(callback: () => void, delay: number | null) {
+  const savedCallback = useRef(callback);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    const tick = () => savedCallback.current();
+
+    if (delay !== null) {
+      const id = window.setTimeout(tick, delay);
+      return () => window.clearTimeout(id);
     }
   }, [delay]);
 }
