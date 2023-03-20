004de7934cccbebcd4dd6838ac72978065beb276
PokyCookie <0cookieboy0@gmail.com>
2023-03-20 20:46:38 +0900

    skill page

---
+++ src/pages/skills.tsx
@@ -0,0 +1,91 @@
+import "../styles/pages/skills.scss";
+import AutoCard from "../components/cards/autoCard";
+import srcHTML5 from "../assets/img/icons8-html-5-480.png";
+import srcCSS3 from "../assets/img/icons8-css3-480.png";
+import srcJS from "../assets/img/icons8-javascript-480.png";
+import srcTS from "../assets/img/icons8-typescript-480.png";
+import srcREACT from "../assets/img/icons8-react-400.png";
+import srcNODE from "../assets/img/icons8-nodejs-480.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { Variants, motion } from "framer-motion";
+import { useState } from "react";
+
+const srcArr = [
+  [srcHTML5, srcCSS3, srcJS],
+  [srcTS, srcREACT, srcNODE],
+];
+
+// const srcArr = imgArr.map((sub) => {
+//   return sub.map((e, i) => {
+//     return { src: e, id: i };
+//   });
+// });
+
+const btnVariants: Variants = {
+  off: {
+    opacity: 0,
+  },
+  on: {
+    opacity: 1,
+  },
+  hover: {
+    scale: 1.3,
+  },
+  tap: {
+    scale: 1.1,
+  },
+};
+
+interface IProps {
+  pagesRef: React.MutableRefObject<null[] | HTMLDivElement[]>;
+}
+
+function SkillPage(props: IProps) {
+  const [srcIndex, setSrcIndex] = useState(0);
+
+  const srcIndexHandler = (type: "inc" | "dec") => {
+    const maxIndex = srcArr.length - 1;
+    let tmpIndex = srcIndex + (type === "inc" ? 1 : -1);
+    if (tmpIndex > maxIndex) {
+      tmpIndex = 0;
+    } else if (tmpIndex < 0) {
+      tmpIndex = maxIndex;
+    }
+    setSrcIndex(tmpIndex);
+  };
+
+  return (
+    <div className="skill__page">
+      <div className="card__area">
+        <motion.button
+          onClick={() => srcIndexHandler("dec")}
+          initial="off"
+          whileInView="on"
+          whileHover="hover"
+          whileTap="tap"
+          viewport={{ once: false, amount: 0.2 }}
+          variants={btnVariants}
+        >
+          <FontAwesomeIcon icon={faAngleLeft} />
+        </motion.button>
+        {srcArr[srcIndex].map((e, i) => {
+          return <AutoCard key={i} src={e} delay={i * 0.2} />;
+        })}
+        <motion.button
+          onClick={() => srcIndexHandler("inc")}
+          initial="off"
+          whileInView="on"
+          whileHover="hover"
+          whileTap="tap"
+          viewport={{ once: false, amount: 0.2 }}
+          variants={btnVariants}
+        >
+          <FontAwesomeIcon icon={faAngleRight} />
+        </motion.button>
+      </div>
+    </div>
+  );
+}
+
+export default SkillPage;
