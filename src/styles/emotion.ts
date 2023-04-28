d00c0b1d8a355d50215083a6a83c6635900c7c81
cookie <0cookieboy0@gmail.com>
2023-04-29 01:43:04 +0900

    feat: skillCard, skillCard action

--- src/styles/emotion.ts
+++ src/styles/emotion.ts
@@ -11,3 +11,24 @@ export const pageCSS = css({
   alignItems: "center",
   position: "relative",
 });
+
+export const cardCSS = css({
+  borderRadius: "10px",
+  backdropFilter: "blur(10px)",
+  backgroundColor: "rgba(255, 255, 255, 0.6)",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  userSelect: "none",
+  boxSizing: "border-box",
+  padding: "10px",
+});
+
+export const flipCardCSS = css({
+  width: "100%",
+  height: "100%",
+  position: "absolute",
+  top: 0,
+  left: 0,
+  backfaceVisibility: "hidden",
+});
